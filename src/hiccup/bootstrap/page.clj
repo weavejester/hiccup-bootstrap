444d6a9017c451037b57760f5b06f70429465496
James Reeves <jreeves@weavejester.com>
2012-04-10 20:23:55 +0100

    Added fixed-layout function

--- src/hiccup/bootstrap/page.clj
+++ src/hiccup/bootstrap/page.clj
@@ -8,3 +8,8 @@
    (include-css "/bootstrap/css/bootstrap.css")
    (include-css "/bootstrap/css/bootstrap-responsive.css")
    (include-js  "/bootstrap/js/bootstrap.js")))
+
+(defn fixed-layout
+  "Encase its contents in a fixed layout container."
+  [& content]
+  [:div.container content])
