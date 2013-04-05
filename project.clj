d52ac0b38de365f95f2b77f88a422fc67b5179db
James Reeves <jreeves@weavejester.com>
2013-04-05 14:35:49 +0100

    Updated dependencies to latest versions

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [hiccup "1.0.0"]
-                 [compojure "1.1.1"]])
+                 [hiccup "1.0.3"]
+                 [compojure "1.1.5"]])
