014679db3a9bb068b521ff5cb1589301336a32b2
James Reeves <jreeves@weavejester.com>
2013-04-05 14:36:23 +0100

    Released 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hiccup-bootstrap "0.1.1"
+(defproject hiccup-bootstrap "0.1.2"
   :description "Hiccup functions for Twitter's Bootstrap"
   :url "https://github.com/weavejester/hiccup-bootstrap"
   :license {:name "Eclipse Public License"
